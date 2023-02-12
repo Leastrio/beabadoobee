8631793a8ab9ba2f35acea2f56d68814bc65eb18
Leastrio <jacob@leastr.io>
2023-02-12 03:55:13 -0700

    FIX MEOWTOP

--- lib/beabadoobee/fun.ex
+++ lib/beabadoobee/fun.ex
@@ -25,6 +25,6 @@ defmodule Beabadoobee.Fun do
   end
 
   def handle_meow(%Nostrum.Struct.Message{} = msg) do
-    Beabadoobee.Database.Members.upsert_meow(msg.guild_id, msg.author.id)
+    Beabadoobee.Database.Meows.upsert_meow(msg.guild_id, msg.author.id)
   end
 end
